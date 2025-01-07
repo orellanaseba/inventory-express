f0efc26ae8926cf27cd100b2cdccc99ccfff14c8
Sebastián <orellanasebastian2002@gmail.com>
2025-01-06 23:55:39 -0300

    some

--- databases/pool.js
+++ databases/pool.js
@@ -4,7 +4,10 @@ require("dotenv").config()
 const { Pool } = pg
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL || "postgres://postgres:sebachiche@localhost:5432/inventory-express"
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    }
 })
 
 module.exports = pool;
